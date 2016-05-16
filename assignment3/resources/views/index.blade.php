cfcdade8797e730c6760d19ac994ab60b135ba46
Dag-Roger <dag.roger.e@gmail.com>
2016-05-16 18:27:19 +0200

    meny eks

--- assignment3/resources/views/index.blade.php
+++ assignment3/resources/views/index.blade.php
@@ -5,5 +5,15 @@
 @stop
 
 @section('content')
+    <header>
+        <a href="index.php" class="logo">Online newspaper</a>
+        <nav>
+            <ul>
+                <li><a href="index.php">Front page</a></li>
+                <li><a href="register.php">Register</a></li>
+                <li><a href="login.php">Log in</a></li>
+            </ul>
+        </nav>
+    </header>
     <h1>This is the frontpage</h1>
 @stop
