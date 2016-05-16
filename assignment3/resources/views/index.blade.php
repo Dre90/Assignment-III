4e1db86b12df2dd4d657fcacc032ea29bba1932d
Sjur Sutterud Sagen <sssagen@hotmail.com>
2016-05-16 20:18:15 +0200

    merge fix

--- assignment3/resources/views/index.blade.php
+++ assignment3/resources/views/index.blade.php
@@ -5,20 +5,5 @@
 @stop
 
 @section('content')
-<<<<<<< HEAD
-    
-    <h1>This is the frontpage</h1>
-=======
-    <header>
-        <a href="index.php" class="logo">Assignemt 3</a>
-        <nav>
-            <ul>
-                <li><a href="index.php">Front page</a></li>
-                <li><a href="register.php">Register</a></li>
-                <li><a href="login.php">Log in</a></li>
-            </ul>
-        </nav>
-    </header>
 
->>>>>>> origin/master
 @stop
